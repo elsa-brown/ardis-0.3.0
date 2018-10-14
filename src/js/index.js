ac2a60aefced8fa1e369dad989b2245d6efd45ba
Elsa Brown <elsambrown@gmail.com>
2018-10-14 14:21:04 -0400

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,108 +1,102 @@
-const init = () => {
-	const audio = new Audio();
-	audio.src = '../../assets/audio/i-built-utopia.mp3';
-	audio.controls = true;
-	document.body.append(audio);
+// adapted from https://github.com/axismaps/contours/blob/master/contours.js
+const d3 = require('d3');
 
-	let context = new (window.AudioContext || window.webkitAudioContext)();
-	let analyser = context.createAnalyser();
+// canvas on which contours will be drawn
+let contourCanvas = document.createElement('canvas');
+contourCanvas.id = 'contours';
+let contourContext;
+let buffer = 5;
 
-	window.addEventListener('load', () => {
-	let source = context.createMediaElementSource(audio);
-	source.connect(analyser);
-	analyser.connect(context.destination);
-}, false);
+// invisible canvas to which terrain tiles will be drawn for calculations to be made
+let demCanvas = document.createElement('canvas');
+let demContext;
+let demImageData;
+let demData;
 
-	const canvas = document.getElementById('canvas');
-	canvas.height = 400;
-	canvas.width = 400;
-	const canvasCtx = canvas.getContext('2d');
+contourContext = contourCanvas.getContext('2d');
+demContext = demCanvas.getContext('2d');
 
-	analyser.fftSize = 1024; // size of Fast Fourier Transform used to get frequency domain data (determines window size)
-	let bufferLength = analyser.frequencyBinCount; // half the fftSize - the number of data points available for visualization
-	// buffer is the asset in memory that holds audio data
-	let dataArray = new Uint8Array(bufferLength); // an array of unsigned integers the length of frequencyBinCount
-	console.log('dataArray: ', dataArray);
+let mapNode = d3.select('#map').node();
+let width = mapNode.offsetWidth + 2 * buffer;
+let height = mapNode.offsetHeight + 2 + buffer;
+contourCanvas.width = width;
+contourCanvas.height = height;
+demCanvas.width = width;
+demCanvas.height = height;
 
-	// timeDomain(canvas, canvasCtx, analyser, bufferLength, dataArray);
-	frequencyDomain(canvas, canvasCtx, analyser, bufferLength, dataArray);
+let path = d3.geoPath().contect(contourContext);
+let svgPath = d3.geoPath();
+
+let min,
+		max,
+		interval,
+		majorInterval = 0;
+		thresholds,
+		contour = d3.contours().size([width, height]),
+		contoursGeoData;
+
+let wait;
+
+// style variables
+let type = 'lines',
+		unit = 'ft';
+
+let lineWidth = .75,
+		lineWidthMajor = 1.5,
+		lineColor = 'blue';
+
+let highlightColor = 'rgba(177, 174, 164, .5)',
+		shadowColor = 'red',
+		shadowSize = 2;
+
+let colorType = 'none',
+		solidColor = '#fffcfa',
+		hypsoColor = d3.scaleLinear()
+			.domain([0, 6999])
+			.range(['#486341', '#e5d9c9'])
+			.interpolate(d3.interpolateHcl),
+		oceanColor = 'orange',
+		bathyColorType = 'none',
+		bathyColor = d3.scaleLinear()
+			.domain([0, 6000])
+			.range(['#315d9b', '#d5f2ff']);
+
+let contourSVG;
+
+window.onresize = () => {
+	width = mapNode.offsetWidth + 2 * buffer;
+	height = mapNode.offsetHeight + 2 * buffer;
+	contourCanvas.width = width;
+	contourCanvas.height = height;
+	demCanvas.width = width;
+	demCanvas.height = height;
+	contour.size([width, height]);
+	clearTimeout(wait);
+	wait = setTimeout(getRelief, 500);
 }
 
-const timeDomain = (canvas, canvasCtx, analyser, bufferLength, dataArray) => {
 
-	const draw = () => {
-		requestAnimationFrame(draw);
 
-		analyser.getByteTimeDomainData(dataArray); // Copies the current frequency data into the unsigned byte array (Uint8Array) passed in
-		console.log('dataArray: ', dataArray);
 
-		canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-		canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-		canvasCtx.lineWidth = 2;
-		canvasCtx.strokeStyle = 'rgb(0, 0, 0)'; // this could increment to change the color of the line as they progress
 
-		let sliceWidth = canvas.width * 1.0 / bufferLength;
-		let x = 0;
 
-		for (var i = 0; i < bufferLength; i++) {
-			let v = dataArray[i] / 128.0;
-			let y = v * canvas.height / 2;
 
-			if (i === 0) {
-				canvasCtx.moveTo(x, y);
-			} else {
-				canvasCtx.lineTo(x, y);
-			}
 
-			x += sliceWidth;
-		}
 
-		canvasCtx.lineTo(canvas.width, canvas.height / 2);
-		canvasCtx.stroke();
-	}
 
-	draw();
 
-};
 
-const frequencyDomain = (canvas, canvasCtx, analyser, bufferLength, dataArray) => {
-	// console.log('dataArray: ', dataArray);
-	// analyser.minDecibels = -90;
-	// analyser.maxDecibels = -10;
 
-	canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-	const draw = () => {
-		requestAnimationFrame(draw);
 
-		analyser.getByteFrequencyData(dataArray); // Copies the current frequency data into the unsigned byte array (Uint8Array) passed in
 
-		// this happens every frame. The dataArray updates with the set of frequency data for each sample frame, at each point in time
-		// We would want to provide a stream of this data to Leaflet/d3contour in order to get a map that redraws each frame as the song/animation progresses in time
-		console.log('dataArray: ', dataArray);
 
-		canvasCtx.fillStyle = 'rgb(253, 197, 190)';
-		canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-		let barWidth = (canvas.width / bufferLength) * 2.5 - 1; // bar width calculated as a ratio of canvas width / number of data points (frequency data);
-		let barHeight;
-		let x = 0;
 
-		// This loop redraws the canvas in each animation frame
-		// Instead of drawing, we want to visualize the data via d3, but we likely still want to use requestAnimationFrame in order to make a visualization that changes over time
-		for (var i = 0; i < bufferLength; i++) {
-			barHeight = dataArray[i]; // barHeight === frequency data point
 
-			canvasCtx.fillStyle = 'rgb(' + barHeight + ', 140, 140)'; // style based on barheight
-			canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
 
-			x += barWidth;
-		}
-	};
 
-	draw();
 
-}
 
-init();
+
